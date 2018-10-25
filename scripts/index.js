e0180556866decb45dc4d385984f1be365b067dd
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-25 15:32:20 -0400

    adds attempts to styling the marker pin after setting a zoom of 13

--- scripts/index.js
+++ scripts/index.js
@@ -19,8 +19,8 @@ function getLocation(cb) {
         navigator.geolocation.getCurrentPosition(function(location) {
             lats = location.coords.latitude;
             longs = location.coords.longitude;
-          console.log("Your Lat/Lon: " + lats + "/" + longs);
-          cb({'myLat': lats, 'myLon': longs});
+            console.log("Your Lat/Lon: " + lats + "/" + longs);
+            cb({'myLat': lats, 'myLon': longs});
         })}    
 }
 
@@ -104,12 +104,16 @@ function getPinInfo(results) {
 }
 
 
-function mapPins(pins) {      // TBD - placeholder for function to display pins on map - replace with Kyle's work
+function mapPins(pins) {      
+    // First rewrites the map centered on the zipcode that is put in the 
+    // for field
+    // then takes the pins info and uses the long/lat to set the pins
+    // the pins will show specific info when clicked
     
     const address = formZipcode.value;
     console.log('drawing map');
     let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
+        zoom: 13,
         //mapTypeId: google.maps.mapTypeId.ROADMAP
     });
     let geocoder = new google.maps.Geocoder();
@@ -127,9 +131,11 @@ function mapPins(pins) {      // TBD - placeholder for function to display pins 
         });
 
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            
             return function (){
-                infowindow.setContent(pins[i].eventName);
+                infowindow.setContent('<h3 style="color:red">' + pins[i].eventName + '</h3>' + 'this is a generic description');
                 infowindow.open(map,marker);
+                // marker.setAnimation(google.maps.Animation.BOUNCE);
             }
         })(marker, i));
     }
